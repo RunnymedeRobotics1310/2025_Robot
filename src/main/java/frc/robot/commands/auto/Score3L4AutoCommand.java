5eab9a176f1dfd44bfdf03c080421e8d21c582ea
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2025-03-12 18:31:15 -0400

    fix a sign error

--- src/main/java/frc/robot/commands/auto/Score3L4AutoCommand.java
+++ src/main/java/frc/robot/commands/auto/Score3L4AutoCommand.java
@@ -2,14 +2,11 @@ package frc.robot.commands.auto;
 
 import static frc.robot.Constants.AutoConstants.FieldLocation.*;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.commands.coral.MoveToCoralPoseCommand;
 import frc.robot.commands.coral.intake.IntakeCoralCommand;
-import frc.robot.commands.coral.intake.PlantCoralCommand;
 import frc.robot.commands.swervedrive.*;
 import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -19,7 +16,8 @@ public class Score3L4AutoCommand extends SequentialCommandGroup {
 
   double speed = 3;
 
-  public Score3L4AutoCommand(SwerveSubsystem swerve, CoralSubsystem coral, LimelightVisionSubsystem vision, double delay) {
+  public Score3L4AutoCommand(
+      SwerveSubsystem swerve, CoralSubsystem coral, LimelightVisionSubsystem vision, double delay) {
     super();
 
     addCommands(new SetAutoGyroCommand(swerve, 180));
@@ -28,19 +26,21 @@ public class Score3L4AutoCommand extends SequentialCommandGroup {
     addCommands(new DriveToFieldLocationCommand(swerve, PRE_SCORE_LEFT_4));
 
     // TODO: scoring block commented out until nathan & jeff fix it
-//    addCommands(
-//            new DriveToScorePositionCommand(swerve, vision, PRE_SCORE_LEFT_4, true)
-//                    .alongWith(new MoveToCoralPoseCommand(Constants.CoralConstants.CoralPose.SCORE_L4, coral).withTimeout(2)));
-//    addCommands(new WaitCommand(0.5));
-//    addCommands(new PlantCoralCommand(coral).deadlineFor(new NullDriveCommand(swerve)));
-    //fixme: remove this when above is done
+    //    addCommands(
+    //            new DriveToScorePositionCommand(swerve, vision, PRE_SCORE_LEFT_4, true)
+    //                    .alongWith(new
+    // MoveToCoralPoseCommand(Constants.CoralConstants.CoralPose.SCORE_L4, coral).withTimeout(2)));
+    //    addCommands(new WaitCommand(0.5));
+    //    addCommands(new PlantCoralCommand(coral).deadlineFor(new NullDriveCommand(swerve)));
+    // fixme: remove this when above is done
     addCommands(new WaitCommand(4).deadlineFor(new NullDriveCommand(swerve)));
 
-    addCommands(new DriveRobotOrientedOmegaCommand(swerve, 0.20, 0, 0).withTimeout(1));
+    addCommands(new DriveRobotOrientedOmegaCommand(swerve, -0.20, 0, 0).withTimeout(1));
     addCommands(new MoveToCoralPoseCommand(Constants.CoralConstants.CoralPose.COMPACT, coral));
 
     addCommands(new DriveToFieldLocationCommand(swerve, blueLeftOuterStation, 0.25));
-    addCommands(new DriveRobotOrientedOmegaCommand(swerve, 0.25, 0, 0)
+    addCommands(
+        new DriveRobotOrientedOmegaCommand(swerve, 0.25, 0, 0)
             .deadlineFor(new IntakeCoralCommand(coral, false)));
 
     // ------------------------the-did-it-work-line------------------------------------
