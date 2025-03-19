84a2290bc38aab0393a9588a04ecae1ef172d6f3
carson <carson.honeyman@gmail.com>
2025-03-19 15:32:30 -0400

    Added timer to auto climb command - NEEDS TESTING

--- src/main/java/frc/robot/commands/climb/AutoClimbCommand.java
+++ src/main/java/frc/robot/commands/climb/AutoClimbCommand.java
@@ -1,11 +1,13 @@
 package frc.robot.commands.climb;
 
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.commands.LoggingCommand;
 import frc.robot.subsystems.ClimbSubsystem;
 
 public class AutoClimbCommand extends LoggingCommand {
 
   private final ClimbSubsystem climbSubsystem;
+  Timer climbTimer = new Timer();
 
   public AutoClimbCommand(ClimbSubsystem climbSubsystem) {
 
@@ -16,25 +18,38 @@ public class AutoClimbCommand extends LoggingCommand {
 
   @Override
   public void initialize() {
-
     logCommandStart();
+    climbTimer.stop();
+    climbTimer.reset();
   }
 
   @Override
   public void execute() {
 
-    if (climbSubsystem.isCageInPosition()) {
+    if (climbSubsystem.isCageInPosition() && !climbTimer.isRunning()) {
+
+      climbTimer.start();
+
+    } else if (!climbSubsystem.isCageInPosition() && climbTimer.isRunning()) {
+
+      climbTimer.stop();
+      climbTimer.reset();
+
+    } else if (climbTimer.hasElapsed(0.3)) {
+
       climbSubsystem.setClimbDeployed(true);
     }
   }
 
   @Override
   public boolean isFinished() {
-    return climbSubsystem.isCageInPosition();
+    return climbSubsystem.isCageInPosition() && climbTimer.hasElapsed(0.3);
   }
 
   @Override
   public void end(boolean interrupted) {
+    climbTimer.stop();
+    climbTimer.reset();
     logCommandEnd(interrupted);
   }
 }
