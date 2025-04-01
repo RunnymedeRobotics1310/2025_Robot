eb807ac323dd242504ec1611f755917e4773122e
Jeff Zakrzewski <jeff@zakr.ca>
2025-04-01 15:30:25 -0400

    Update to RunnymedeSwerve 3.2.0

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 308;
-  public static final String GIT_SHA = "84798457cb76c6fddd87a71bcfeaf5980df29a65";
-  public static final String GIT_DATE = "2025-04-01 14:43:40 EDT";
+  public static final int GIT_REVISION = 311;
+  public static final String GIT_SHA = "1d1da0e8a2bf698732ec18d0552d19bb99c722a1";
+  public static final String GIT_DATE = "2025-04-01 15:29:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-01 15:15:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1743534951085L;
+  public static final String BUILD_DATE = "2025-04-01 15:30:06 EDT";
+  public static final long BUILD_UNIX_TIME = 1743535806636L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
